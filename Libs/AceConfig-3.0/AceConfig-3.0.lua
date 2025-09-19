39b724ea8ab8c4ead228cf8ebdd52e3beccbe4fb
Kirill Serogodsky <serogodsky.kirill@gmail.com>
2025-09-20 02:15:59 +0300

    comment self check

--- Libs/AceConfig-3.0/AceConfig-3.0.lua
+++ Libs/AceConfig-3.0/AceConfig-3.0.lua
@@ -44,9 +44,10 @@ local pcall, error, type, pairs = pcall, error, type, pairs
 -- local AceConfig = LibStub("AceConfig-3.0")
 -- AceConfig:RegisterOptionsTable("MyAddon", myOptions, {"/myslash", "/my"})
 function AceConfig:RegisterOptionsTable(appName, options, slashcmd)
-	if self == AceConfig then
-		error([[Usage: RegisterOptionsTable(appName, options[, slashcmd]): 'self' - use your own 'self']], 2)
-	end
+	-- We realy need this?
+	-- if self == AceConfig then
+	-- 	error([[Usage: RegisterOptionsTable(appName, options[, slashcmd]): 'self' - use your own 'self']], 2)
+	-- end
 	local ok,msg = pcall(cfgreg.RegisterOptionsTable, self, appName, options)
 	if not ok then error(msg, 2) end
 	
